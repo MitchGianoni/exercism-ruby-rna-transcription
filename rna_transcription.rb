443d59e1f44fe2877bff1ba7fcdf4638706bb578
MitchGianoni <mitchgianoni@yahoo.com>
2018-05-24 21:52:24 -0400

    got through five tests, found another failure

--- rna_transcription.rb
+++ rna_transcription.rb
@@ -1,3 +1,17 @@
-class RnaTranscription
-
+class Complement
+  def self.of_dna(x)
+    if x == 'G'
+      return 'C'
+    elsif x == 'C'
+      return 'G'
+    elsif x == 'T'
+      return 'A'
+    elsif x == 'A'
+      return 'U'
+    end
+  end
 end
+
+# class RnaTranscription
+
+# end
